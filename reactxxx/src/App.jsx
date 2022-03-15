dc4b64d62b15c0e30d3d64b66302b12e025c94f3
TheWisePigeon <zozozozeph@gmail.com>
2022-03-15 20:08:04 +0000

    getting started to create the online gallery

--- reactxxx/src/App.jsx
+++ reactxxx/src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
 import './App.css'
 import './Styles.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
       <div>
-        <Header/>
-        <Footer/>
+        
       </div>
       
       
