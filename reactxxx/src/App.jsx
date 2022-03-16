11884f6b525877d4ea1d2883a87c8beaa6fe4e03
TheWisePigeon <zozozozeph@gmail.com>
2022-03-16 22:03:41 +0000

    made Artwork component

--- reactxxx/src/App.jsx
+++ reactxxx/src/App.jsx
@@ -1,15 +1,17 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Artwork from './components/Artwork'
 
 function App() {
 
   return (
-      <div>
-        <Header/>
-        <Footer/>
-      </div>
-      
-      
+    <div>
+      <Header />
+      <Artwork src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBKArlMzyX01_ZjRpDgDqueaCBqMLYWKQDXg&usqp=CAU" alt="bruh" title="DANK MEME" description="Just a random meme" />
+      <Footer />
+    </div>
+
+
   )
 }
 
--- reactxxx/src/components/Artwork.jsx
+++ reactxxx/src/components/Artwork.jsx
@@ -4,8 +4,13 @@ function Artwork(props) {
 
     return (
         <div>
-            
+            <div className=" w-40 h-40 rounded-full">
+                <img  src={props.src} alt={props.alt} />
+            </div>
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
         </div>
     )
     
 }
+export default Artwork
