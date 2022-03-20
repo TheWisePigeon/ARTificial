4d61e07eee2fe11380119b9fab7477936f4fb83f
TheWisePigeon <zozozozeph@gmail.com>
2022-03-20 23:31:50 +0000

    random commit again

--- reactxxx/src/components/Artworks.jsx
+++ reactxxx/src/components/Artworks.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import Artwork from "./Artwork";
+import axios from 'axios'
+
+axios.get('https://artificialbackend.herokuapp.com/arts').then((res)=>{
+  console.log(res.data);
+})
 
 function Artworks() {
+
+    function createArtwork(artworks) {
+        artworks.map()
+    }
+
     return (
         <div className=" flex flex-wrap overflow-hidden" >
             <Artwork title="test" description="Just a description" src="https://picsum.photos/200" />
