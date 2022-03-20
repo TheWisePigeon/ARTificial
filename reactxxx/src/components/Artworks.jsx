1338fd3b853ac026937edefe250308ca8765f310
TheWisePigeon <zozozozeph@gmail.com>
2022-03-20 23:40:18 +0000

    Update Artworks.jsx
    working, just have to do some aditional front end

--- reactxxx/src/components/Artworks.jsx
+++ reactxxx/src/components/Artworks.jsx
@@ -2,18 +2,24 @@ import React from "react";
 import Artwork from "./Artwork";
 import axios from 'axios'
 
-axios.get('https://artificialbackend.herokuapp.com/arts').then((res)=>{
-  console.log(res.data);
+let content = await axios.get('https://artificialbackend.herokuapp.com/arts').then((res)=>{
+  return(res.data);
 })
+console.log(content);
 
 function Artworks() {
 
     function createArtwork(artworks) {
-        artworks.map()
+        return(
+            <Artwork src={artworks.src} title={artworks.title} description={artworks.desc}  />
+        ) 
+        
+        
     }
 
     return (
         <div className=" flex flex-wrap overflow-hidden" >
+            {/* <Artwork title="test" description="Just a description" src="https://picsum.photos/200" />
             <Artwork title="test" description="Just a description" src="https://picsum.photos/200" />
             <Artwork title="test" description="Just a description" src="https://picsum.photos/200" />
             <Artwork title="test" description="Just a description" src="https://picsum.photos/200" />
@@ -24,8 +30,12 @@ function Artworks() {
             <Artwork title="test" description="Just a description" src="https://picsum.photos/200" />
             <Artwork title="test" description="Just a description" src="https://picsum.photos/200" />
             <Artwork title="test" description="Just a description" src="https://picsum.photos/200" />
-            <Artwork title="test" description="Just a description" src="https://picsum.photos/200" />
-            <Artwork title="test" description="Just a description" src="https://picsum.photos/200" />
+            <Artwork title="test" description="Just a description" src="https://picsum.photos/200" /> */}
+            {
+                content.map(
+                    artwork=> createArtwork(artwork)
+                )
+            }
         </div>
     )
 }
