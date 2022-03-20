7bbc7912254b912914be2660212a18b4f8b204ff
TheWisePigeon <zozozozeph@gmail.com>
2022-03-20 23:23:57 +0000

    added axios

--- reactxxx/src/main.jsx
+++ reactxxx/src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './tailwind.css'
 import App from './App'
+import axios from 'axios'
+
+axios.get('https://artificialbackend.herokuapp.com/arts').then((res)=>{
+  console.log(res);
+})
 
 ReactDOM.render(
   <React.StrictMode>
